c86d0db6b03b2e3d7aad056d934803cb8208b916
changminkangkk <cmkang1211@gmail.com>
2024-06-06 14:25:31 +0900

    Time: 110 ms (17.55%), Space: 17 MB (82.56%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.py
@@ -0,0 +1,8 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.slots = [0, big, medium, small]
+
+    def addCar(self, car_type: int) -> bool:
+        self.slots[car_type] = count = max(self.slots[car_type] - 1, -1)
+        return count > -1
