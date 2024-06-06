e3125ce8b9ff78e65b2875f5bece6d75330fd8ae
changminkangkk <cmkang1211@gmail.com>
2024-06-06 15:40:40 +0900

    Time: 35 ms (51.05%), Space: 16.5 MB (77%) - LeetHub

---
+++ 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        x=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        a=""
+        for i in firstWord:
+            a=a+str(x.index(i))
+        
+        b=""
+        for i in secondWord:
+            b=b+str(x.index(i))
+
+        c=""
+        for i in targetWord:
+            c=c+str(x.index(i))
+        if int(a)+int(b)==int(c):
+            return True
+        return False
